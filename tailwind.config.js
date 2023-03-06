73a46043ba39db2eb567ab6f16912f5abd2e4aae
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2023-03-07 00:46:38 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend:{
+      colors: {
+        "primary" : "#A555EC",
+        "secondary" : "#F3CCFF",
+        "third" : "#FFFFD0",
+        "link" : "#1D77FF"
+      }
+    }
+   
+  },
+  plugins: [],
+}
